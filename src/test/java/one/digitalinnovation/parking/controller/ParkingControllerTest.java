764257774591c9564b7f1e0cc33d8047c75f48e1
Roberta de Siqueira <designer.roberta@gmail.com>
2022-09-10 21:51:21 -0300

    testdocker

--- src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
+++ src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
@@ -1,33 +1,31 @@
 package one.digitalinnovation.parking.controller;
 
-import io.restassured.RestAssured;
-import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.restassured.RestAssured;
+import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ParkingControllerTestIT {
+class ParkingControllerTest extends AbstractContainerBase {
 
   @LocalServerPort
   private int randomPort;
 
   @BeforeEach
   public void setUpTest() {
-    System.out.println(randomPort);
     RestAssured.port = randomPort;
   }
 
   @Test
-  void whenfindAllThenCheckResult() {
+  void whenFindAllThenCheckResult() {
     RestAssured.given()
+            .auth().basic("user", "12345")
             .when()
             .get("/parking")
             .then()
@@ -36,7 +34,6 @@ class ParkingControllerTestIT {
 
   @Test
   void whenCreateThenCheckIsCreated() {
-
     var createDTO = new ParkingCreateDTO();
     createDTO.setColor("AMARELO");
     createDTO.setLicense("WRT-5555");
@@ -45,6 +42,7 @@ class ParkingControllerTestIT {
 
     RestAssured.given()
             .when()
+            .auth().basic("user", "12345")
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(createDTO)
             .post("/parking")
